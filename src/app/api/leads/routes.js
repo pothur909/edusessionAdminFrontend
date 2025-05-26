e36ecdbf8ad27117b64049dbe3b59e06f2ab8352
unknown <saichanduallamgari@gmail.com>
2025-05-26 11:23:44 +0530

    Initial commit

--- src/app/api/leads/routes.js
+++ src/app/api/leads/routes.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const leadController = require('./controller');
+// const express = require('express');
+// const router = express.Router();
+// const leadController = require('./controller');
 
-// View all leads
-router.get('/viewleads', leadController.viewLeads);
+// // View all leads
+// router.get('/viewleads', leadController.viewLeads);
 
-// View single lead
-router.get('/viewleads/:id', leadController.viewSingleLead);
+// // View single lead
+// router.get('/viewleads/:id', leadController.viewSingleLead);
 
-// Add new lead
-router.post('/addlead', leadController.addLead);
+// // Add new lead
+// router.post('/addlead', leadController.addLead);
 
-// Edit lead
-router.put('/editlead/:id', leadController.editLead);
+// // Edit lead
+// router.put('/editlead/:id', leadController.editLead);
 
-// Delete lead
-router.delete('/deletelead/:id', leadController.deleteLead);
+// // Delete lead
+// router.delete('/deletelead/:id', leadController.deleteLead);
 
-module.exports = router; 
+// module.exports = router; 
